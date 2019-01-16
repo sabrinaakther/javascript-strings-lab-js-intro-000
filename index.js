14a7704d71436303ac5541e0aa2991d8da444d9d
Sabrina Akther <sabrinasobhan.92@gmail.com>
2019-01-16 21:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest ="Hello,"+"Hello,Neil deGrasse Tyson!";
+var specialGuest ="Hello,Neil deGrasse Tyson";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "Let's talk about space.";
 
